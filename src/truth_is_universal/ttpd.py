11c942eabb30e6890167ed9079a7acdce7b47cc5
Hichem Ammar Khodja <hichem.ammarkhodja@orange.com>
2025-02-24 11:25:25 +0100

    Complete setup to test TTPD

--- src/truth_is_universal/ttpd.py
+++ src/truth_is_universal/ttpd.py
@@ -4,22 +4,14 @@ import os
 import os.path as osp
 from typing import Iterable
 import numpy as np
-from sklearn.linear_model import LogisticRegression
 import torch
 from tqdm import tqdm
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import pandas as pd
-
+from .regressor import TTPD as TTPDRegressor, learn_truth_directions
 
 
 STORAGE_FOLDER = os.envrion.get('STORAGE_FOLDER', "./")
-
-class Hook:
-    def __init__(self):
-        self.out = None
-
-    def __call__(self, module, module_inputs, module_outputs):
-        self.out, _ = module_outputs
 
 def load_dataset(dataset_name) -> tuple[list[str], list[int], list[float]]:
     """
@@ -30,7 +22,7 @@ def load_dataset(dataset_name) -> tuple[list[str], list[int], list[float]]:
     labels = dataset['label'].tolist()
     polarity = -1.0 if 'neg_' in dataset_name else 1.0
     polarities = [polarity]*len(labels)
-    return statements, labels, polarities
+    return statements, labels, polarities, len(labels)
 
 def extract_layers(model) -> torch.nn.Module:
     pass
@@ -56,8 +48,17 @@ def load_statements(datasets: str | list[str]) -> tuple[Iterable[str], list[int]
             dataset_name = os.path.relpath(file_path, 'datasets').replace('.csv', '')
             datasets.append(dataset_name)
 
-    statements, labels, polarities = zip(*[(statement, label, polarity) for dataset in datasets for statement, label, polarity in load_dataset(dataset)])
-    return statements, labels, polarities, datasets
+    datasets_sizes = []
+
+    all_statements, all_labels, all_polarities  = [],[],[]
+    
+    for dataset in datasets:
+        for statements, labels, polarities, size in load_dataset(dataset):
+            datasets_sizes.append(size)
+            all_statements.extend(statements)
+            all_labels.extend(labels)
+            all_polarities.extend(polarities)
+    return all_statements, all_labels, all_polarities, datasets, datasets_sizes
 
 def batched(iterable : Iterable, batch_size: int) -> Iterable[list]:
     l = []
@@ -69,38 +70,52 @@ def batched(iterable : Iterable, batch_size: int) -> Iterable[list]:
     yield l
 
 
-class TTPDRegressor:
-    def __init__(self):
-        self.t_g = None
-        self.polarity_direc = None
-        self.LR = None
+def compute_layer2perf(activations: np.ndarray, labels: np.ndarray) -> np.ndarray:
+    within_class_variances = []
+    between_class_variances = []
+    for layer_nr in range(activations.shape[1]):
+        acts = activations[:, layer_nr] 
+        # Calculate means for each class
+        false_stmnt_ids = labels == 0
+        true_stmnt_ids = labels == 1
 
-    def from_data(acts_centered, acts, labels, polarities):
-        probe = TTPD()
-        probe.t_g, _ = learn_truth_directions(acts_centered, labels, polarities)
-        probe.t_g = probe.t_g.numpy()
-        probe.polarity_direc = learn_polarity_direction(acts, polarities)
-        acts_2d = probe._project_acts(acts)
-        probe.LR = LogisticRegression(penalty=None, fit_intercept=True)
-        probe.LR.fit(acts_2d, labels.numpy())
-        return probe
-    
-    def pred(self, acts):
-        acts_2d = self._project_acts(acts)
-        return torch.tensor(self.LR.predict(acts_2d))
-    
-    def _project_acts(self, acts):
-        proj_t_g = acts.numpy() @ self.t_g
-        proj_p = acts.numpy() @ self.polarity_direc.T
-        acts_2d = np.concatenate((proj_t_g[:, None], proj_p), axis=1)
-        return acts_2d
+        false_acts = acts[false_stmnt_ids]
+        true_acts = acts[true_stmnt_ids]
+
+        mean_false = false_acts.mean(dim=0)
+        mean_true = true_acts.mean(dim=0)
+
+        # Calculate within-class variance
+        within_class_variance_false = false_acts.var(dim=0).mean()
+        within_class_variance_true = true_acts.var(dim=0).mean()
+        within_class_variances.append((within_class_variance_false + within_class_variance_true).item() / 2)
+
+        # Calculate between-class variance
+        overall_mean = acts.mean(dim=0)
+        between_class_variances.append(((mean_false - overall_mean).pow(2) 
+                                        + (mean_true - overall_mean).pow(2)).mean().item() / 2)
+
+    layer2perf = np.array(between_class_variances) / np.array(within_class_variances)
+    return layer2perf
+
+def center_activations(activations: torch.Tensor, dataset_sizes: list[int]) -> torch.Tensor:
+    activations = activations.clone()
+    cumsum_sizes = np.array(dataset_sizes).cumsum()
+    for i in range(len(dataset_sizes)):
+        start = cumsum_sizes[i]
+        end = start + dataset_sizes[i]
+        activations[start:end] = activations[start:end] - activations[start:end].mean(0).unsqueeze(0)
+    return activations
 
 class TTPD:
     FOLDER_NAME = "TTPDTruthDirections"
-    def __init__(self, model_name : str, precision=16):
+    def __init__(self, model_name : str, precision=16, model=None, tokenizer=None, device_map="auto"):
         self.model_name = model_name
-        self.truth_vector: list[float] = None
-        self.polarity_vector: list[float] = None
+        self.t_g: torch.Tensor = None
+        self.t_p: torch.Tensor = None
+        self.model = model
+        self.tokenizer = tokenizer
+        self.device_map = device_map
 
         assert precision in [16,32]
         self.precision = precision
@@ -118,69 +133,77 @@ class TTPD:
             raise Exception('Call self.build() first!')
         
         vectors = json.load(open(self.path))
-        self.truth_vector = vectors['truth']
-        self.polarity_vector = vectors['polarity']
+        self.t_g = torch.tensor(vectors['truth'])
+        self.t_p = torch.tensor(vectors['polarity'])
+        self.best_layer = vectors["best_layer"]
     
     @torch.no_grad()
-    def build(self, batch_size: int, device_map: str, force=False) -> None:
+    def build(self, batch_size: int, force=False) -> None:
         if self.is_built() and not force:
             raise Exception("TTDP Truth Directions were already computed for the model %s \n Use force=True to rebuild and replace previous vectors" % self.model_name)
         
-        model = AutoModelForCausalLM.from_pretrained(self.model_name, device_map=device_map, torch_dtype=self.torch_dtype)
+        # Step 1: Collect activations
+        model = AutoModelForCausalLM.from_pretrained(self.model_name, device_map=self.device_map, torch_dtype=self.torch_dtype)
         tokenizer = AutoTokenizer.from_pretrained(self.model_name)
         tokenizer.padding_side = 'left'
 
         device = next(model.named_parameters().values()).device
 
-        statements, labels, datasets_names = load_statements(["cities", "neg_cities", "sp_en_trans", "neg_sp_en_trans", "inventors", "neg_inventors", "animal_class",
-                  "neg_animal_class", "element_symb", "neg_element_symb", "facts", "neg_facts"]) # The same dataset used in the paper for lie detection.
+        dataset_list = ["cities", "neg_cities", "sp_en_trans", "neg_sp_en_trans", "inventors", "neg_inventors", "animal_class",
+                  "neg_animal_class", "element_symb", "neg_element_symb", "facts", "neg_facts"]
+        statements, labels, polarities, _, dataset_sizes = load_statements(dataset_list) # The same dataset used in the paper for lie detection.
         batched_statements_iter = batched(statements, batch_size)
         activations = []
         for batch in tqdm(batched_statements_iter, "Generating activations", total=len(statements) // batch_size):
             acts = collect_activations(model, tokenizer, batch, device)
             activations.append(acts)
 
-        # print("Saving activations...")
         activations = torch.cat(activations, dim=0)
-        # for layer in range(activations.shape[1]):
-        #     torch.save(activations[:, layer], osp.join(self.path, f"layer_{layer}.pt"))
-        # print("Activations saved!")
 
-
-        # Find best layer
-        within_class_variances = []
-        between_class_variances = []
+        # Step 2: Find best layer
         labels = np.array(labels)
-        for layer_nr in range(activations.shape[1]):
-            acts = activations[:, layer_nr] 
-            # Calculate means for each class
-            false_stmnt_ids = labels == 0
-            true_stmnt_ids = labels == 1
-
-            false_acts = acts[false_stmnt_ids]
-            true_acts = acts[true_stmnt_ids]
-
-            mean_false = false_acts.mean(dim=0)
-            mean_true = true_acts.mean(dim=0)
-
-            # Calculate within-class variance
-            within_class_variance_false = false_acts.var(dim=0).mean()
-            within_class_variance_true = true_acts.var(dim=0).mean()
-            within_class_variances.append((within_class_variance_false + within_class_variance_true).item() / 2)
-
-            # Calculate between-class variance
-            overall_mean = acts.mean(dim=0)
-            between_class_variances.append(((mean_false - overall_mean).pow(2) 
-                                            + (mean_true - overall_mean).pow(2)).mean().item() / 2)
-
-        layer2perf = np.array(between_class_variances) / np.array(within_class_variances)
+        best_layer_datasets = ['cities', 'neg_cities', 'sp_en_trans', 'neg_sp_en_trans']
+        cumsum_sizes = np.array(dataset_sizes).cumsum ()
+        mask = np.zeros(activations.shape, dtype=np.bool)
+        for ds in best_layer_datasets:
+            idx = dataset_list.index(ds)
+            start = cumsum_sizes[idx]
+            end = start + dataset_sizes[idx]
+            mask[start:end] = 1
+        best_layer_activations = activations[mask]
+        best_layer_labels = labels[mask]
+        layer2perf = compute_layer2perf(best_layer_activations, best_layer_labels)
         best_layer = np.argmax(layer2perf)
 
-        # Compute truth directions
+        # Step 3: Compute truth directions
         activations_best_layer = activations[:, best_layer]
-        activations_best_layer_centered = activations_best_layer - activations_best_layer.mean(1)
-        acts_centered, _, labels, polarities = collect_training_data(cv_train_sets, train_set_sizes, model_family,
-                                                          model_size, model_type, layer)
+        activations_best_layer_centered = center_activations(activations_best_layer, dataset_sizes)
         
-        # Fit model
-        t_g, t_p = learn_truth_directions(acts_centered, labels, polarities)
+        regressor = TTPDRegressor.from_data(activations_best_layer_centered, activations_best_layer, labels, polarities)
+
+        # Step 4: Save truth directions
+        t_g, t_p = regressor.t_g.tolist(), regressor.polarity_direc.tolist()
+
+        with open(self.path, "w") as f:
+            json.dump({
+                "truth": t_g,
+                "polarity": t_p,
+                "best_layer": best_layer 
+            }, f)
+        print("Truth directions saved in %s" % self.path)
+        print("Build finished!")
+
+    def project(self, statements: list[str]):
+        if self.model is None:
+            self.model = AutoModelForCausalLM.from_pretrained(self.model_name, device_map=self.device_map, torch_dtype=self.torch_dtype)
+        if self.tokenizer is None:
+            tokenizer = AutoTokenizer.from_pretrained(self.model_name)
+        tokenizer.padding_side = 'left'
+        device = next(self.model.named_parameters().values()).device
+
+        outputs = collect_activations(self.model, tokenizer, statements, device)
+        activations = outputs[:, self.best_layer]
+        proj_t_g = activations @ self.t_g
+        proj_p = activations @ self.t_p.T
+        acts_2d = torch.cat((proj_t_g[:, None], proj_p), dim=1)
+        return acts_2d
